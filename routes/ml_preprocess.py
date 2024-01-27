db136ba0ec8028f7d2fd3c8915ea4b4fc8dc5027
hjck4433 <hjck4433@gmail.com>
2024-01-27 22:41:29 +0900

    전처리 진행중

--- routes/ml_preprocess.py
+++ routes/ml_preprocess.py
@@ -2,6 +2,12 @@ import pandas as pd
 import os
 from konlpy.tag import Okt, Hannanum
 import re
+from collections import Counter
+from nltk import ngrams
+from sklearn.preprocessing import StandardScaler
+from sklearn.preprocessing import MinMaxScaler
+import matplotlib.pyplot as plt
+import seaborn as sns
 
 
 # 데이터 전처리
@@ -28,7 +34,7 @@ def preprocess_genre(genre):
 def preprocess_title(title, stopwords):
     # 하나의 숫자로만 되어 있는 경우 ex) 65, 1917
     if title.isdigit():
-        return [title]
+        return [title], Counter()
 
     # 특수문자 제거
     title = re.sub(r'[^\w\s]', '', title)
@@ -38,7 +44,12 @@ def preprocess_title(title, stopwords):
     tokens = tokenizer.morphs(title)
     # 불용어 제거
     tokens = [token for token in tokens if token not in stopwords]
-    return tokens
+    # return tokens
+
+    # Word Frequency Calculation
+    word_freq = Counter(tokens)
+
+    return tokens, word_freq
 
 # 영화 줄거리
 def preprocess_plot(plot, stopwords):
@@ -52,7 +63,16 @@ def preprocess_plot(plot, stopwords):
     # 불용어 제거
     tokens = [token for token in tokens if token not in stopwords]
 
-    return tokens
+    # return tokens
+
+    # Word Frequency Calculation
+    word_freq = Counter(tokens)
+
+    # N-gram Extraction (for example, using bigrams)
+    bigrams = list(ngrams(tokens, 2))
+
+    return tokens, word_freq, bigrams
+
 
 # 숫자 데이터
 def preprocess_numeric_columns(data):
@@ -64,6 +84,46 @@ def preprocess_numeric_columns(data):
             data[col] = pd.to_numeric(data[col].str.replace(',', ''), errors='coerce')
         else:
             data[col] = data[col].astype(float)
+
+        # Scaling
+        if col == 'movieScore':
+            scaler = StandardScaler()  # Use StandardScaler for 'movieScore'
+        else:
+            scaler = MinMaxScaler()  # Use MinMaxScaler for other columns
+
+        scaled_col = scaler.fit_transform(data[[col]])
+
+        # 스케일링 전 후 시각화
+        # fig, axes = plt.subplots(1, 2, figsize=(12, 6))
+        # sns.histplot(data[col], ax=axes[0], kde=True, color='blue', bins=20)
+        # axes[0].set_title(f'Distribution of {col} Before Scaling')
+        # axes[0].set_xlabel(col)
+        # axes[0].set_ylabel('Frequency')
+        #
+        # sns.histplot(pd.Series(scaled_col.flatten()), ax=axes[1], kde=True, color='red', bins=20)
+        # axes[1].set_title(f'Distribution of {col} After Scaling')
+        # axes[1].set_xlabel(col)
+        # axes[1].set_ylabel('Frequency')
+        #
+        # plt.tight_layout()
+        # plt.show()
+
+
+        # 스케일링 전 후 비교
+        print(f"Descriptive Statistics for '{col}':")
+        print("Before Scaling:")
+        print(data[col].describe())
+        print("After Scaling:")
+        print(pd.Series(scaled_col.flatten()).describe())
+        print("-----------------------------------")
+
+        data[col] = scaled_col  # Fit and transform on each numerical column
+
+        # 각 컬럼이 가우시안 분포인지 확인
+        if (data[col].skew() < 0.5) and (data[col].kurtosis() < 3):
+            print(f"The distribution of '{col}' is approximately Gaussian.")
+        else:
+            print(f"The distribution of '{col}' is not Gaussian.")
 
     return data
 
@@ -91,10 +151,15 @@ data = preprocess_numeric_columns(data)
 data = preprocess_date_column(data)
 
 # 토큰화
-data['movieTitle_tokens'] = data['movieTitle'].apply(preprocess_title, args=(stopwords,))
+# data['movieTitle_tokens'] = data['movieTitle'].apply(preprocess_title, args=(stopwords,))
+# data['movieGenre_tokens'] = data['movieGenre'].apply(preprocess_genre)
+# data['movieDirector_tokens'] = data['movieDirector'].apply(preprocess_names)
+# data['movieActors_tokens'] = data['movieActors'].apply(preprocess_names)
+data['movieTitle_tokens'], data['movieTitle_word_freq'] = zip(*data['movieTitle'].apply(preprocess_title, args=(stopwords,)))
 data['movieGenre_tokens'] = data['movieGenre'].apply(preprocess_genre)
 data['movieDirector_tokens'] = data['movieDirector'].apply(preprocess_names)
 data['movieActors_tokens'] = data['movieActors'].apply(preprocess_names)
+data['moviePlot_tokens'], data['moviePlot_word_freq'], data['moviePlot_bigrams'] = zip(*data['moviePlot'].apply(preprocess_plot, args=(stopwords,)))
 
 # 새 컬럼 생성
 data['audience_density'] = data['audience'] / data['screening']
@@ -105,6 +170,43 @@ data['views'] = 0
 # 조회 대비 북마크 수
 data['like_view_ratio'] = data['likes'] / data['views']
 data['moviePlot_tokens'] = data['moviePlot'].apply(preprocess_plot, args=(stopwords,))
+
+unique_directors = set()
+unique_actors = set()
+unique_genres = set()
+
+for directors in data['movieDirector_tokens']:
+    unique_directors.update(directors)
+
+for actors in data['movieActors_tokens']:
+    unique_actors.update(actors)
+
+for genres in data['movieGenre_tokens']:
+    unique_genres.update(genres)
+
+# 이진 컬럼 생성
+binary_columns = []
+
+# 각 유형에 따른 접두사
+director_prefix = 'director_'
+actor_prefix = 'actor_'
+genre_prefix = 'genre_'
+
+for director in unique_directors:
+    column_name = director_prefix + director
+    binary_columns.append(data['movieDirector_tokens'].apply(lambda x: 1 if director in x else 0).rename(column_name))
+
+for actor in unique_actors:
+    column_name = actor_prefix + actor
+    binary_columns.append(data['movieActors_tokens'].apply(lambda x: 1 if actor in x else 0).rename(column_name))
+
+for genre in unique_genres:
+    column_name = genre_prefix + genre
+    binary_columns.append(data['movieGenre_tokens'].apply(lambda x: 1 if genre in x else 0).rename(column_name))
+
+binary_df = pd.concat(binary_columns, axis=1)
+data = pd.concat([data, binary_df], axis=1)
+
 
 
 data.to_csv(output_path, index=False, encoding='utf-8-sig')
