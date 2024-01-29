5e7c80ff38753fe47d7f307159188e00f71514e7
hjck4433 <hjck4433@gmail.com>
2024-01-29 17:51:50 +0900

    우선순위 적용

--- routes/ml_cosign_similarity.py
+++ routes/ml_cosign_similarity.py
@@ -3,6 +3,7 @@ import os
 from sklearn.metrics.pairwise import cosine_similarity
 from sklearn.feature_extraction.text import TfidfVectorizer
 import time
+import numpy as np
 
 # 작업 시작
 start_time = time.time()
@@ -21,6 +22,9 @@ weights = {
     'director_weight': 0.2
 }
 
+# 영화 점수의 표준화된 점수의 60번째 백분위수를 계산
+percentile_60th = np.percentile(data['movieScore'], 60)
+
 # 각 카테고리별 가중 유사도를 계산하는 함수 정의
 def calculate_weighted_similarity(column_name, user_preference, weight):
     user_pref_vector = tfidf_vectorizer.transform([user_preference])
@@ -34,10 +38,36 @@ tfidf_vectorizer.fit(all_text_data)
 
 # 사용자의 선호도 설정
 user_preferences = {
-    'preferActors': '',
-    'preferDirectors': '',
-    'preferGenres': '로맨스'
+    'preferActors': '마동석, 손석구, 키아누 리브스',
+    'preferDirectors': '김성수, 이상용',
+    'preferGenres': '드라마, 범죄, 액션',
+    'movieId': '1,2,3,4'
 }
+
+# 북마크된 영화 ID를 추출하고 처리
+bookmarked_movie_ids = user_preferences.pop('movieId', '').split(',')
+# 북마크된 영화 ID가 있는지 확인
+if bookmarked_movie_ids and bookmarked_movie_ids[0]:
+    # 북마크된 영화 ID를 정수형으로 변환
+    bookmarked_movie_ids = list(map(int, bookmarked_movie_ids))
+    # 북마크된 영화의 특징을 추출하고 사용자 선호도에 추가
+    for feature in ['movieGenre', 'movieActors', 'movieDirector']:
+        # 북마크된 영화의 중복되지 않는 특징을 추출하고, 쉼표로 구분하여 결합
+        aggregated_feature = ', '.join(data[data['movieId'].isin(bookmarked_movie_ids)][feature].dropna().unique())
+        # 특징에 따라 사용자 선호도 키를 조정하여 user_preferences에 추가 (Actors에는 이미 s가 붙어있음)
+        if feature.endswith('Actors'):
+            user_preference_key = 'prefer' + feature[5:]
+        else:
+            user_preference_key = 'prefer' + feature[5:] + 's'
+        # 특징 데이터가 있는 경우에만 사용자 선호도를 업데이트
+        if aggregated_feature:
+            if user_preference_key in user_preferences and user_preferences[user_preference_key]:
+                user_preferences[user_preference_key] += ', ' + aggregated_feature
+            else:
+                user_preferences[user_preference_key] = aggregated_feature
+
+# 북마크된 영화를 제외한 데이터셋 생성
+data = data[~data['movieId'].isin(bookmarked_movie_ids)]
 
 # 각 카테고리별 가중 유사도 계산
 genre_similarity = calculate_weighted_similarity('movieGenre', user_preferences['preferGenres'], weights['genre_weight'])
@@ -49,7 +79,12 @@ total_similarity = genre_similarity + actor_similarity + director_similarity
 
 # 데이터프레임에 총 유사도 점수를 추가하고 이를 기준으로 정렬
 data['similarity_score'] = total_similarity
-recommended_movies = data.sort_values(by='similarity_score', ascending=False)
+
+# 60번째 백분위수 이상의 영화에 더 높은 우선순위를 할당
+data['priority'] = data['movieScore'].apply(lambda x: 1 if x >= percentile_60th else 0)
+
+# 정렬 조건을 우선순위, 유사도 순으로 설정
+recommended_movies = data.sort_values(by=['priority', 'similarity_score'], ascending=[False, False])
 
 # 작업 끝
 end_time = time.time()
@@ -58,5 +93,5 @@ end_time = time.time()
 elapsed_time = end_time - start_time
 
 # 상위 추천 영화 출력
-print(recommended_movies[['movieId', 'movieTitle','movieScore', 'similarity_score']].head(10))
+print(recommended_movies[['movieId', 'movieTitle', 'movieScore', 'similarity_score']].head(10))
 print(f"The code took {elapsed_time} seconds to run.")
